60fa0374e5faecaa52d7ad7d5c72bd4816d1ef40
Julian <juek3y@gmail.com>
2020-11-23 20:42:30 +0100

    fade out testing

--- js/fading.js
+++ js/fading.js
@@ -1,15 +1,23 @@
+function fade_in() {
 // Fade In
-$(document).ready(function() {
-      $('body').css('display', 'none');
-      $('body').fadeIn(350);
-});
+    $(document).ready(function() {
+        $('body').css('display', 'none');
+        $('body').fadeIn(350);
+    });
+}
 
-// Fade Out
-$(document).ready(function() {
-      $(".clk").click(function() {
-            $("*").fadeOut();
-      });
-});
-
+function fade_out() {
+    // Fade Out
+    $(document).ready(function() {
+        $(".clk").click(function() {
+                $("*").fadeOut();
+        });
+    });
+}
 // Delay
-function delay (URL) { setTimeout( function() { window.location = URL }, 350 ); }
+function delay (URL) {
+    setTimeout( function() {
+         window.location = URL
+         $("*").fadeOut();
+    }, 350 ); 
+}

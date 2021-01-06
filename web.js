0213e843f181e73fde7ff5d38fdcc383971b4ab6
JueK3y <juek3y@gmail.com>
2021-01-06 11:47:02 +0100

    Tidied up a bit

--- web.js
+++
@@ -1,9 +0,0 @@
-const imagemin = require('imagemin');
-const imageminWebp = require('imagemin-webp');
-
-imagemin(['img/*'], {
-  destination: 'compressed_images',
-  plugins: [imageminWebp({quality: 100})]
-}).then(() => {
-  console.log('Done!');
-});

68dde3e64098245a56c7eb1c9de6926e4b1f996c
JueK3y <juek3y@gmail.com>
2021-03-08 13:37:33 +0100

    Updated header

--- src/js/index-reval.js
+++ src/js/index-reval.js
@@ -68,23 +68,3 @@ TweenMax.from(".nav-btn", 1, {
         x: 20,
         ease: Expo.easeInOut
 })
-
-
-$(document).ready(function() {  
-    $('input[type="checkbox"]').val(this.checked);
-
-    $('input[type="checkbox"]').change(function() {
-          if(this.checked) {
-                $('.logo, a').css('color', 'black');
-                $('#active').css('color', '#C2C5A2');
-                $('.nav-wrapper').css("display", "block");
-                $('.nav-wrapper').css("left", "0");
-                $('.so-me-resp').css("display", "flex");
-          }
-          else {
-                $('.logo, a').css('color', 'white');
-                $('.nav-wrapper').css("display", "none");
-                $('.so-me-resp').css("display", "none");
-          }
-    });
-});

5b68561e067d320d6ea353603a7ea2046870a552
JueK3y <54510690+JueK3y@users.noreply.github.com>
2021-03-08 21:43:44 +0100

    updated content

---
+++ src/js/hyperlink.js
@@ -0,0 +1,15 @@
+document.getElementById('one').onclick = () => {
+    delay('settings#settings')
+}
+
+document.getElementById('two').onclick = () => {
+    delay('settings#story')
+}
+
+document.getElementById('biographie').onclick = () => {
+    delay('biographie')
+}
+
+document.getElementById('contact').onclick = () => {
+    delay('contact')
+}

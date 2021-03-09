48eb8bfd47fb883da8e3e4e23775237777e8f72d
JueK3y <juek3y@gmail.com>
2021-03-09 05:52:29 +0100

    Updated index

--- src/js/hyperlink.js
+++ src/js/hyperlink.js
@@ -6,10 +6,31 @@ document.getElementById('two').onclick = () => {
     delay('settings#story')
 }
 
+document.getElementById('three').onclick = () => {
+    delay('settings#faq')
+}
+
+document.getElementById('four').onclick = () => {
+    delay('settings#contact')
+}
+
+document.getElementById('five').onclick = () => {
+    delay('settings#impressum')
+}
+
+document.getElementById('six').onclick = () => {
+    delay('settings#datenschutz')
+}
+
+
 document.getElementById('biographie').onclick = () => {
-    delay('biographie')
+    delay('bio')
 }
 
 document.getElementById('contact').onclick = () => {
     delay('contact')
+}
+
+document.getElementById('language').onclick = () => {
+    delay('/en/index')
 }

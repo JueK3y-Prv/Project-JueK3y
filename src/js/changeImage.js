1f787f9297d1b1917b82ab93be82644fff3b5236
JueK3y <juek3y@gmail.com>
2021-05-19 18:00:54 +0200

    Image Changer is now fully working

--- src/js/changeImage.js
+++ src/js/changeImage.js
@@ -1,54 +1,22 @@
-var pI = 2
-var cI = 3
-var nI = 4
-
 document.getElementById('next-button').onclick = function() {
 
-    if (nI <= 4) {
-        currentImage = 'image-' + (cI)
-        nextImage = 'image-' + (nI)
+    prevCurrentImageID = document.querySelector('.image').id
+    prevCurrentImageNumber = parseInt(prevCurrentImageID.slice(-1))
+    // alert(prevCurrentImageNumber)
+
+    if (prevCurrentImageNumber <= 3) {
     
-        document.getElementById(currentImage).id = nextImage
+        document.getElementById('image-number').textContent = '00' + (prevCurrentImageNumber + 1)
+        document.getElementById(prevCurrentImageID).id = 'image-' + (prevCurrentImageNumber + 1)    
 
-
-        currentImageNumber = '00' + (nI)
-
-        document.getElementById('image-number').textContent = currentImageNumber
-        
-        if (pI == 4) {
-            pI = 0
-        }
-
-        pI++
-
-        cI++
-        nI++
-
-        console.log(pI, cI, nI)
     }
 
     else {
-        pI = 3
-        cI = 4
-        nI = 1
-        
-        currentImage = 'image-' + (cI)
-        nextImage = 'image-' + (nI)
-    
-        document.getElementById(currentImage).id = nextImage
-    
+ 
+        var test = document.getElementById('image-number').textContent = '00' + (prevCurrentImageNumber - 3)
+        console.log(test)
+        document.getElementById(prevCurrentImageID).id = 'image-' + (prevCurrentImageNumber - 3)  
 
-        currentImageNumber = '00' + (nI)
-
-        document.getElementById('image-number').textContent = currentImageNumber
-
-        cI = 0
-
-        pI ++
-        cI++
-        nI++
-
-        console.log(pI, cI, nI)
     }
 }
 

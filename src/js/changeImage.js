ab7f57c82321d2bd3d2b3f3b881c4bb0bdd013fd
JueK3y | Personal Account <54510690+JueK3y-Prv@users.noreply.github.com>
2021-05-25 15:58:43 +0200

    Update changeImage.js

--- src/js/changeImage.js
+++ src/js/changeImage.js
@@ -1,31 +1,34 @@
 function nextImage() {
-    prevCurrentImageID = document.querySelector('.image').id
-    prevCurrentImageNumber = parseInt(prevCurrentImageID.slice(-1))
+    currentImageID = document.querySelector('.image').id
+    currentImageNumber = parseInt(currentImageID.slice(-1))
     
-    if (a && prevCurrentImageNumber <= 3) {
-        document.getElementById('image-number').textContent = '00' + (prevCurrentImageNumber + 1)
-        document.getElementById(prevCurrentImageID).id = 'image-' + (prevCurrentImageNumber + 1)    
+    if (a && currentImageNumber <= 3) {
+        document.getElementById('image-number').textContent = '00' + (currentImageNumber + 1)
+        document.getElementById(currentImageID).id = 'image-' + (currentImageNumber + 1)    
     }
-    else if (b && prevCurrentImageNumber >= 2) {
-        document.getElementById('image-number').textContent = '00' + (prevCurrentImageNumber - 1)
-        document.getElementById(prevCurrentImageID).id = 'image-' + (prevCurrentImageNumber - 1)   
+    else if (b && currentImageNumber >= 2) {
+        document.getElementById('image-number').textContent = '00' + (currentImageNumber - 1)
+        document.getElementById(currentImageID).id = 'image-' + (currentImageNumber - 1)   
     }
     else {
-        document.getElementById('image-number').textContent = '00' + (prevCurrentImageNumber - b)
-        document.getElementById(prevCurrentImageID).id = 'image-' + (prevCurrentImageNumber - b)  
+        document.getElementById('image-number').textContent = '00' + (currentImageNumber - c)
+        document.getElementById(currentImageID).id = 'image-' + (currentImageNumber - c)  
     }
+
+    a = false
+    b = false
 }
 
 
 document.getElementById('next-button').onclick = function() {
-    nextImage(a, c = 3)    
+    nextImage(a = true, c = 3)    
 }
 
 
 document.getElementById('prev-button').onclick = function() {
-    nextImage(b, c = -3)    
+    nextImage(b = true, c = -3)    
 }
 
 setInterval(function() {
-    nextImage(a, c = 3)    
+    nextImage(a = true, c = 3)    
 }, 9000); 

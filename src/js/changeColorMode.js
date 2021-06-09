d5b790e2090ff2b45ac8f87e3b19a58bb5c1796c
JueK3y | Personal Account <54510690+JueK3y-Prv@users.noreply.github.com>
2021-06-09 11:52:25 +0200

    Create changeColorMode.js
    The file will replace changeLightDark.js.

---
+++ src/js/changeColorMode.js
@@ -0,0 +1,44 @@
+const theme = localStorage.getItem("theme");
+const iconSun = document.getElementById('sun-icon')
+const iconMoon = document.getElementById('moon-icon')
+const image = document.getElementById('image-filter')
+
+
+function modeToLight() {
+    iconMoon.style.display = 'inline'
+    iconSun.style.display = 'none'
+    image.style.filter = 'invert(0)'
+    document.documentElement.style.filter = 'invert(0)'
+}
+function modeToDark() {
+    iconMoon.style.display = 'none'
+    iconSun.style.display = 'inline'
+    image.style.filter = 'invert(1)'
+    document.documentElement.style.filter = 'invert(1)'
+}
+
+
+if (theme) {
+    if (theme == "dark") {
+        modeToDark()
+    }
+    else {
+        modeToLight()
+    }
+}
+else if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
+    modeToDark()
+}
+else {
+    modeToLight()
+}
+
+
+iconSun.onclick = () => {
+    localStorage.setItem("theme", "light");
+    modeToLight();
+};
+iconMoon.onclick = () => {
+    localStorage.setItem("theme", "dark");
+    modeToDark();
+};

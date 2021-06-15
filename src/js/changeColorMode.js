3537b24b6a0ab60256893977bfe09eb9f37fe356
JueK3y | Personal Account <54510690+JueK3y-Prv@users.noreply.github.com>
2021-06-15 12:10:43 +0200

    Update changeColorMode.js

--- src/js/changeColorMode.js
+++ src/js/changeColorMode.js
@@ -1,6 +1,3 @@
-const circleType = new CircleType(document.getElementById('rotated')).radius(60)
-circleType.forceWidth(true)
-
 const theme = localStorage.getItem("theme");
 const iconSun = document.getElementById('sun-icon')
 const iconMoon = document.getElementById('moon-icon')

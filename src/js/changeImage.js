a705e5463e0d089a36edf42e027e854d4776dd08
Julian Kennedy <julian.kennedy@esg.de>
2021-10-26 14:26:26 +0200

    Updated image distortion path

--- src/js/changeImage.js
+++ src/js/changeImage.js
@@ -1,18 +1,31 @@
-/*function nextImage() {
+var img1 = "/src/img/img_one.jpg"
+var img2 = "/src/img/img_two.jpg"
+
+var hoverDistort = new hoverEffect({
+    parent: document.querySelector('.image'),
+    intensity: 0.4,
+    image1: img1,
+    image2: img2,
+    displacementImage: "/src/img/displacement/4.png" //4.png 7.jpg 14.jpg
+});
+
+
+function nextImage() {
     currentImageID = document.querySelector('.image').id
     currentImageNumber = parseInt(currentImageID.slice(-1))
     
     if (a && currentImageNumber <= 3) {
         document.getElementById('image-number').textContent = '00' + (currentImageNumber + 1)
-        document.getElementById(currentImageID).id = 'image-' + (currentImageNumber + 1)    
+        document.getElementById(currentImageID).id = 'image-' + (currentImageNumber + 1)
+        alert('image-' + (currentImageNumber + 1))
     }
     else if (b && currentImageNumber >= 2) {
         document.getElementById('image-number').textContent = '00' + (currentImageNumber - 1)
-        document.getElementById(currentImageID).id = 'image-' + (currentImageNumber - 1)   
+        document.getElementById(currentImageID).id = 'image-' + (currentImageNumber - 1)
     }
     else {
         document.getElementById('image-number').textContent = '00' + (currentImageNumber - c)
-        document.getElementById(currentImageID).id = 'image-' + (currentImageNumber - c)  
+        document.getElementById(currentImageID).id = 'image-' + (currentImageNumber - c)
     }
 
     a = false
@@ -30,14 +43,5 @@ document.getElementById('prev-button').onclick = function() {
 
 setInterval(function() {
     nextImage(a = true, c = 3)    
-}, 9000);  */
+}, 9000);
 
-
-
-var hoverDistort = new hoverEffect({
-    parent: document.querySelector('.image-filter'),
-    intensity: 0.4,
-    image1: "/src/img/img_one.jpg",
-    image2: "/src/img/img_two.jpg",
-    displacementImage: "/src/img/displacement/4.png" //4.png 7.jpg 14.jpg
-});

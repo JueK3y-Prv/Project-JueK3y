de2e1c0790216d3a8cdacfd4fd07d284f7c66759
Julian Kennedy <julian.kennedy@esg.de>
2021-10-26 13:23:31 +0200

    Change submission page

--- src/js/submissions.js
+++
@@ -1,6 +0,0 @@
-
-(function() {
-    if (document.referrer && document.location.host && document.referrer.match(new RegExp("^https?://" + document.location.host))) {
-        document.getElementById("back-link").setAttribute("href", document.referrer);
-    }
-})();

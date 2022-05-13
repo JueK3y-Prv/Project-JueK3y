aa9dd6a4e7050abae671bb7b5624f74ae5ca8101
JueK3y <juek3y@gmail.com>
2022-05-13 13:04:09 +0200

    Updated gender validation

--- src/js/business.js
+++ src/js/business.js
@@ -1,54 +1,57 @@
 // Wrong gender selected
 $(document).ready(function() {
     $(".gender-wrong").click(function(){
-        $('.gender-t-m').show();
-        $('.gender-t-m-a').hide();      
-        $('.gender-t-f').show(); 
-        $('.gender-t-f-a').hide();  
-        $('.gender-wrong').hide();  
-        $('input[type="radio"]').prop('checked', false);
-    });
+        $('.gender-t-m').show()
+        $('.gender-t-m-a').hide()      
+        $('.gender-t-f').show() 
+        $('.gender-t-f-a').hide()  
+        $('.gender-wrong').hide()  
+        $('input[type="radio"]').prop('checked', false)
+        $("#anrede-selection").children().prop('selectedIndex', 0)
+    })
     $('input[type="radio"]').click(function() {
         anredeFc($(this).attr('id'))
         //Männlich
         if($(this).attr('id') == 'male') {
-            $('.gender-t-m').show();
-            $('.gender-t-m-a').show();      
-            $('.gender-t-f').hide();  
-            $('.gender-t-f').prop('checked', false); 
-            $('.gender-t-f-a').hide(); 
-            $('.gender-t-f-a').prop('checked', false); 
-            $('.gender-wrong').show();       
+            $('.gender-t-m').show()
+            $('.gender-t-m-a').show()      
+            $('.gender-t-f').hide()  
+            $('.gender-t-f').prop('checked', false) 
+            $('.gender-t-f-a').hide() 
+            $('.gender-t-f-a').prop('checked', false) 
+            $('.gender-wrong').show()       
         }
         //Weiblich
         else if($(this).attr('id') == 'female') {
-            $('.gender-t-m').hide(); 
-            $('.gender-t-m').prop('checked', false);  
-            $('.gender-t-m-a').hide();    
-            $('.gender-t-m-a').prop('checked', false);   
-            $('.gender-t-f').show(); 
-            $('.gender-t-f-a').show();  
-            $('.gender-wrong').show();  
+            $('.gender-t-m').hide() 
+            $('.gender-t-m').prop('checked', false)  
+            $('.gender-t-m-a').hide()    
+            $('.gender-t-m-a').prop('checked', false)   
+            $('.gender-t-f').show() 
+            $('.gender-t-f-a').show()  
+            $('.gender-wrong').show()  
         }
         //None
         else {
-            $('.gender-t-m').hide();
-            $('.gender-t-f-a').hide();     
-            $('.gender-t-f').hide();
-            $('.gender-t-f-a').hide();   
-            $('.gender-wrong').hide();  
+            $('.gender-t-m').hide()
+            $('.gender-t-f-a').hide()     
+            $('.gender-t-f').hide()
+            $('.gender-t-f-a').hide()   
+            $('.gender-wrong').hide()  
         }
-    });
-});
+    })
+})
 
 function anredeFc(gender) {
     let anrede = []
     if (gender === 'female') {
         $("#anrede-selection").attr('class', 'gender-t-f-a')
+        $("#anrede-selection").children().attr('id', 'gender-s-f')
         anrede = ['Frau', 'Frau Dr.', 'Frau Prof.', 'Frau Prof. Dr.']
     }
     else if (gender === 'male') {
         $("#anrede-selection").attr('class', 'gender-t-m-a')
+        $("#anrede-selection").children().attr('id', 'gender-s-m')
         anrede = ['Herr', 'Herr Dr.', 'Herr Prof.', 'Herr Prof. Dr.']
     }
     
@@ -61,49 +64,41 @@ function anredeFc(gender) {
 // Required attribute
 $(document).ready(function() {
     $(".gender-t-m").click(function() {
-        $('.gender-s-m').attr('required', true); 
-    });
-}); 
+        $('#gender-s-m').attr('required', true) 
+    })
+}) 
 $(document).ready(function(){
     $(".gender-t-f").click(function() {
-        $('.gender-s-f').attr('required', true); 
-        $('.gender-i-m-r').attr('required', false); 
-    });
-});
+        $('.gender-s-f').attr('required', true) 
+        $('.gender-i-m-r').attr('required', false) 
+    })
+})
 $(document).ready(function(){
     $(".gender-wrong").click(function() {
-        $('.gender-i-m-r').attr('required', true);
-        $('.gender-s-f').attr('required', false); 
-        $('.gender-s-m').attr('required', false); 
-    });
-});
+        $('.gender-i-m-r').attr('required', true)
+        $('.gender-s-f').attr('required', false) 
+        $('#gender-s-m').attr('required', false) 
+    })
+})
 
 $(document).ready(function() {  
-    $('input[type="checkbox"]').val(this.checked);
+    $('input[type="checkbox"]').val(this.checked)
 
     $('input[type="checkbox"]').change(function() {
         if(this.checked) {
-            $('.nav-wrapper').css("display", "block");
-            $('.so-me-resp').css("display", "flex");
+            $('.nav-wrapper').css("display", "block")
+            $('.so-me-resp').css("display", "flex")
         }
         else {
-            $('.nav-wrapper').css("display", "none");
-            $('.so-me-resp').css("display", "none");
+            $('.nav-wrapper').css("display", "none")
+            $('.so-me-resp').css("display", "none")
         }
-    });
-});
+    })
+})
 
 
 $(document).ready(function() { 
     if ($(window).width() > 915) {
-        $('.resp4').remove();
+        $('.resp4').remove()
     }
-})
-
-
-$(document).ready(() => {
-    $('.button_primary').click(() => {
-        console.log($('.gender-s-m').val())
-        console.log($('.gender-s-f').val())
-    })
 })

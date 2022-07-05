7f6cc3bd5d9b6bba15d643b918866ace7c067d16
JueK3y <juek3y@gmail.com>
2022-07-05 14:09:04 +0200

    Updated analytics

--- src/js/webAnalytics.js
+++ src/js/webAnalytics.js
@@ -13,9 +13,13 @@ if (localStorage.getItem("cookieAccepted") == "true") {
         }
     })
   }
-  
+ 
+if(!sessionStorage.getItem("_swa")&&document.referrer.indexOf(location.protocol+"//"+location.host)!== 0){fetch("https://counter.dev/track?"+new URLSearchParams({referrer:document.referrer,screen:screen.width+"x"+screen.height,user:"juek3y@gmail.com",utcoffset:"2"}))};sessionStorage.setItem("_swa","1");
+
+
+
 function startAnalytics() {
-    /*!function(){var t,o,c,e=window,n=document,r=arguments,a="script",i=["call","cancelAction","config","identify","push","track","trackClick","trackForm","update","visit"],s=function(){var t,o=this,c=function(t){o[t]=function(){return o._e.push([t].concat(Array.prototype.slice.call(arguments,0))),o}};for(o._e=[],t=0;t<i.length;t++)c(i[t])};for(e.__woo=e.__woo||{},t=0;t<r.length;t++)e.__woo[r[t]]=e[r[t]]=e[r[t]]||new s;(o=n.createElement(a)).async=1,o.src="https://static.woopra.com/js/w.js",(c=n.getElementsByTagName(a)[0]).parentNode.insertBefore(o,c)}("woopra");
+    !function(){var t,o,c,e=window,n=document,r=arguments,a="script",i=["call","cancelAction","config","identify","push","track","trackClick","trackForm","update","visit"],s=function(){var t,o=this,c=function(t){o[t]=function(){return o._e.push([t].concat(Array.prototype.slice.call(arguments,0))),o}};for(o._e=[],t=0;t<i.length;t++)c(i[t])};for(e.__woo=e.__woo||{},t=0;t<r.length;t++)e.__woo[r[t]]=e[r[t]]=e[r[t]]||new s;(o=n.createElement(a)).async=1,o.src="https://static.woopra.com/js/w.js",(c=n.getElementsByTagName(a)[0]).parentNode.insertBefore(o,c)}("woopra");
 
     woopra.config({
         domain: "juek3y.com",
@@ -23,7 +27,5 @@ function startAnalytics() {
         download_tracking: true
     });
 
-    woopra.track();*/
-    
-    if(!sessionStorage.getItem("_swa")&&document.referrer.indexOf(location.protocol+"//"+location.host)!== 0){fetch("https://counter.dev/track?"+new URLSearchParams({referrer:document.referrer,screen:screen.width+"x"+screen.height,user:"juek3y@gmail.com",utcoffset:"2"}))};sessionStorage.setItem("_swa","1");
-}
+    woopra.track();
+    }

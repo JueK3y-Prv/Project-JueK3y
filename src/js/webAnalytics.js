c79e0f7369f54c97f632bd9bcc29165e7f4eda50
JueK3y <juek3y@gmail.com>
2022-08-04 12:27:14 +0200

    Correction of access errors

--- src/js/webAnalytics.js
+++ src/js/webAnalytics.js
@@ -1,6 +1,6 @@
 if (localStorage.getItem("cookieAccepted") == "true") {
     startAnalytics()
-    $(document).on("click", "a", function() {
+    /* $(document).on("click", "a", function() {
         var hrID = $(this).attr('id');
         var hrURL = $(this).attr("href");
         var hrText = document.getElementById(hrID).innerText;
@@ -11,15 +11,15 @@ if (localStorage.getItem("cookieAccepted") == "true") {
                 leaving_url: hrURL
             })
         }
-    })
+    }) */
   }
  
-if(!sessionStorage.getItem("_swa")&&document.referrer.indexOf(location.protocol+"//"+location.host)!== 0){fetch("https://counter.dev/track?"+new URLSearchParams({referrer:document.referrer,screen:screen.width+"x"+screen.height,user:"juek3y@gmail.com",utcoffset:"2"}))};sessionStorage.setItem("_swa","1");
+// if(!sessionStorage.getItem("_swa")&&document.referrer.indexOf(location.protocol+"//"+location.host)!== 0){fetch("https://counter.dev/track?"+new URLSearchParams({referrer:document.referrer,screen:screen.width+"x"+screen.height,user:"juek3y@gmail.com",utcoffset:"2"}))};sessionStorage.setItem("_swa","1");
 
 
 
 function startAnalytics() {
-    !function(){var t,o,c,e=window,n=document,r=arguments,a="script",i=["call","cancelAction","config","identify","push","track","trackClick","trackForm","update","visit"],s=function(){var t,o=this,c=function(t){o[t]=function(){return o._e.push([t].concat(Array.prototype.slice.call(arguments,0))),o}};for(o._e=[],t=0;t<i.length;t++)c(i[t])};for(e.__woo=e.__woo||{},t=0;t<r.length;t++)e.__woo[r[t]]=e[r[t]]=e[r[t]]||new s;(o=n.createElement(a)).async=1,o.src="https://static.woopra.com/js/w.js",(c=n.getElementsByTagName(a)[0]).parentNode.insertBefore(o,c)}("woopra");
+    /* !function(){var t,o,c,e=window,n=document,r=arguments,a="script",i=["call","cancelAction","config","identify","push","track","trackClick","trackForm","update","visit"],s=function(){var t,o=this,c=function(t){o[t]=function(){return o._e.push([t].concat(Array.prototype.slice.call(arguments,0))),o}};for(o._e=[],t=0;t<i.length;t++)c(i[t])};for(e.__woo=e.__woo||{},t=0;t<r.length;t++)e.__woo[r[t]]=e[r[t]]=e[r[t]]||new s;(o=n.createElement(a)).async=1,o.src="https://static.woopra.com/js/w.js",(c=n.getElementsByTagName(a)[0]).parentNode.insertBefore(o,c)}("woopra");
 
     woopra.config({
         domain: "juek3y.com",
@@ -27,5 +27,5 @@ function startAnalytics() {
         download_tracking: true
     });
 
-    woopra.track();
-    }
+    woopra.track(); */
+}

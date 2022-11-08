ba479e13811d9d8c279681ca1f5896caf7982ddd
Julian Kennedy <julian.kennedy@esg.de>
2022-11-08 12:53:11 +0100

    Updated content

--- src/js/404.js
+++ src/js/404.js
@@ -1,68 +1,61 @@
 // INFO: GSAP code -!- //
-gsap.from(".menu > div", 1.6, {
+gsap.from('.menu > div', 1.6, {
     opacity: 0,
     y: 60,
     ease: Expo.easeInOut,
     delay: 2.4,
 });
-gsap.from(".brand-logo", 1.6, {
+gsap.from('.brand-logo', 1.6, {
     opacity: 0,
     y: 40,
     ease: Expo.easeInOut,
     delay: 2.4,
 });
-gsap.from(".header > span", 1.5, {
-    top: "-100vh",
-    ease: "bounce.out",
+gsap.from('.header > span', 1.5, {
+    top: '-100vh',
+    ease: 'bounce.out',
     delay: 1,
     stagger: 0.2,
 });
-gsap.to(".footer span", 0.4, {
+gsap.to('.footer span', 0.4, {
     y: -40,
     ease: Expo.easeInOut,
     delay: 2.4,
 });
+gsap.to('.footer-wrapper', 0.5, {
+    display: 'none',
+    delay: 2.6,
+})
 
 // INFO: changeColorMode.js -!- //
-const theme = localStorage.getItem("theme");
+const theme = localStorage.getItem('theme');
 const iconSun = document.getElementById('sun-icon')
 const iconMoon = document.getElementById('moon-icon')
-try {
-    const amountChildren = document.querySelector('.image').children.length
-}
-catch(err) { console.log(err) }
+const linkColor = document.getElementById('footer-link')
 // Needs to be simplified
 
 function modeToLight() {
     iconMoon.style.display = 'inline'
     iconSun.style.display = 'none'
     document.documentElement.style.filter = 'invert(0)'
-    try {
-        for (i = 0; i < amountChildren; i++) {
-            document.querySelector('.image-' + (i + 1)).style.filter = 'invert(0)'
-        }
-    } catch(err) {}
+    linkColor.style.color = '#ad1232'
 }
 function modeToDark() {
     iconMoon.style.display = 'none'
     iconSun.style.display = 'inline'
     document.documentElement.style.filter = 'invert(1)'
-    try {
-        for (i = 0; i < amountChildren; i++) {
-            document.querySelector('.image-' + (i + 1)).style.filter = 'invert(1)'
-        }
-    } catch(err) {}
+    linkColor.style.color = '#5000eb' // It's actually #afff14
 }
 
 if (theme) {
-    if (theme == "dark") {
+    if (theme == 'dark') {
         modeToDark()
     }
     else {
         modeToLight()
     }
 }
-else if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
+else if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
     modeToDark()
 }
 else {
@@ -70,14 +63,14 @@ else {
 }
 
 iconSun.onclick = () => {
-    if (localStorage.getItem("cookieAccepted") == "true") {
-        localStorage.setItem("theme", "light");
+    if (localStorage.getItem('cookieAccepted') == 'true') {
+        localStorage.setItem('theme', 'light');
     }
     modeToLight();
 };
 iconMoon.onclick = () => {
-    if (localStorage.getItem("cookieAccepted") == "true") {
-        localStorage.setItem("theme", "dark");
+    if (localStorage.getItem('cookieAccepted') == 'true') {
+        localStorage.setItem('theme', 'dark');
     }
     modeToDark();
 };
